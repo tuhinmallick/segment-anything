035eadb37f9740e376edc121b36248044c495c0d
Sourcery AI <>
2023-11-07 10:44:40 +0000

    'Refactored by Sourcery'

--- segment_anything/automatic_mask_generator.py
+++ segment_anything/automatic_mask_generator.py
@@ -109,11 +109,11 @@ class SamAutomaticMaskGenerator:
         else:
             raise ValueError("Can't have both points_per_side and point_grid be None.")
 
-        assert output_mode in [
+        assert output_mode in {
             "binary_mask",
             "uncompressed_rle",
             "coco_rle",
-        ], f"Unknown output_mode {output_mode}."
+        }, f"Unknown output_mode {output_mode}."
         if output_mode == "coco_rle":
             from pycocotools import mask as mask_utils  # type: ignore # noqa: F401
 
